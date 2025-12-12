9c4a53910a9263caab0e62694fe2706bbdb482ff
Mikeu Dev <rikiruswandi28@gmail.com>
2025-12-12 11:18:40 +0700

    feat: menambahkan InvoiceResource ke grup navigasi 'Finance'

--- app/Filament/Resources/Invoices/InvoiceResource.php
+++ app/Filament/Resources/Invoices/InvoiceResource.php
@@ -24,6 +24,8 @@ class InvoiceResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
+    protected static string|\UnitEnum|null $navigationGroup = 'Finance';
+
     protected static ?string $recordTitleAttribute = 'number';
 
     public static function form(Schema $schema): Schema
